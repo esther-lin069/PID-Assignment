2e78124b1fc379cce6859f729cbf902d9cd3dff1
Esther-mac <esther_lin@mail.chungyo.net>
2020-08-27 16:30:10 +0800

    add user-show-product

--- shop/app/Product.php
+++ shop/app/Product.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
-    //
+    public function getImageUrlAttribute()
+    {
+        return Storage::disk('public')->url($this->attributes['image']);
+    }
 }
